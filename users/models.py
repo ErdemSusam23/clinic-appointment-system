b48ae180249351c9d29bd8395f927a5f52b6d118
ErdemSusam23 <ersusam2003@gmail.com>
2025-05-04 22:15:58 +0300

    recreate migrations

--- users/models.py
+++ users/models.py
@@ -7,5 +7,21 @@ class User(AbstractUser):
         ('doctor', 'Doctor'),
         ('admin', 'Admin'),
     )
+    
+    # Remove email field
+    email = None
+    
+    # Make first_name and last_name required
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField()
+    
+    # Use username as the USERNAME_FIELD
+    USERNAME_FIELD = 'username'
+    # Required fields for creating a user
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth', 'role']
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
