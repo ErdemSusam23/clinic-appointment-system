0417a1a34b860c0d3165f385b4eeecae917ef56a
ErdemSusam23 <ersusam2003@gmail.com>
2025-05-04 20:46:24 +0300

    Update settings.py

--- clinic_backend/settings.py
+++ clinic_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cjyeaq$^)jj=%t9o(br__y&(&hm8gta6224g=_j@^hp=(ime1r'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-cjyeaq$^)jj=%t9o(br__y&(&hm8gta6224g=_j@^hp=(ime1r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,3 +152,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print(get_random_secret_key())
