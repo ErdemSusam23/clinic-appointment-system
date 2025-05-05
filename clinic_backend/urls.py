54dd37190fba1050f7a80994981890179ee4c6a4
ErdemSusam23 <ersusam2003@gmail.com>
2025-05-05 15:05:29 +0300

    connect root URL "/" with swagger UI
    localhost:8000/ now redirects to swagger UI

--- clinic_backend/urls.py
+++ clinic_backend/urls.py
@@ -5,6 +5,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 # Schema view configuration
 schema_view = get_schema_view(
@@ -22,6 +23,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    # Root URL redirects to API documentation
+    path('', RedirectView.as_view(url='/api/docs/swagger/', permanent=False)),
+    
     # Django Admin
     path('admin/', admin.site.urls),
     
